172fa178394658e46360d8043fa58db9342fdffb
DeWinters <derekwwinters@gmail.com>
2017-02-23 09:02:14 +0000

    Add project files.

---
+++ DFEItechFurniture/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DFEItechFurniture.MvcApplication" Language="C#" %>
---
+++ DFEItechFurniture/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DFEItechFurniture.Startup))]
+namespace DFEItechFurniture
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
