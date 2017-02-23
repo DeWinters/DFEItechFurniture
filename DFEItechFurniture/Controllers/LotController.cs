cfaa1d0e75a5cc4857625d30b233388eb4e8f302
DeWinters <derekwwinters@gmail.com>
2017-02-23 23:09:24 +0000

    dummy Lot slides

---
+++ DFEItechFurniture/Controllers/LotController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DFEItechFurniture.Models;
+
+namespace DFEItechFurniture.Controllers
+{
+    public class LotController : Controller
+    {
+ 
+        public ActionResult ListAllLots()
+        {
+            Lot lot1 = new Lot() { LotId = 1, LotName = "Lot 1", LotImage="image1.jpg", LotDescript = "leather recliner", Exterior = false, Price = 1900.00m };
+            Lot lot2 = new Lot() { LotId = 2, LotName = "Lot 2", LotImage="image2.jpg", LotDescript = "picnic table", Exterior = true, Price = 190.00m };
+            List<Lot> allLots = new List<Lot>();
+            allLots.Add(lot1);
+            allLots.Add(lot2);
+            return View(allLots);
+        }
+    }
+}
---
+++ DFEItechFurniture/Models/Lot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DFEItechFurniture.Models
+{
+    public class Lot
+    {
+        public int LotId { get; set; }
+        public string LotName { get; set; }
+        public string LotImage { get; set; }
+        //public Type LotType {get; set;}
+        public string LotDescript { get; set; }
+        public Boolean Exterior { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
