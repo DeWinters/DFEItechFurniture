096ae61750438bb4e89b32651b77e63c52e56273
DeWinters <derekwwinters@gmail.com>
2017-02-24 01:34:34 +0000

    PartialView model conflict resolved

--- DFEItechFurniture/Controllers/LotController.cs
+++ DFEItechFurniture/Controllers/LotController.cs
@@ -9,15 +9,53 @@ namespace DFEItechFurniture.Controllers
 {
     public class LotController : Controller
     {
- 
-        public ActionResult ListAllLots()
+        [HttpGet]
+        public ActionResult ListLots()
         {
-            Lot lot1 = new Lot() { LotId = 1, LotName = "Lot 1", LotImage="image1.jpg", LotDescript = "leather recliner", Exterior = false, Price = 1900.00m };
-            Lot lot2 = new Lot() { LotId = 2, LotName = "Lot 2", LotImage="image2.jpg", LotDescript = "picnic table", Exterior = true, Price = 190.00m };
+            /** Dummy Return **/
+            Lot lot1 = new Lot() { LotId = 1, LotName = "Lot 1", LotImage = "image1.jpg", LotType = 1, LotDescript = "leather recliner", Exterior = false, Price = 1900.00m };
+            Lot lot2 = new Lot() { LotId = 2, LotName = "Lot 2", LotImage = "image2.jpg", LotType = 3, LotDescript = "picnic table", Exterior = true, Price = 190.00m };
             List<Lot> allLots = new List<Lot>();
             allLots.Add(lot1);
             allLots.Add(lot2);
+
             return View(allLots);
+        }
+        [HttpPost]
+        public ActionResult ListLots(int lotId=0, string lotName=null, int lotType=0, string lotImage=null, string LotDescript=null, Boolean exterior=false, Decimal price=0)
+        {
+            List<Lot> allLots = new List<Lot>();
+            if (lotId != 0) {                                   // GetLotListById (should get single match)
+           // } else if (exterior != false && lotName != null) {  // GetLotByName&Value 
+          //  } else if (lotName != null && lotType != 0) {       // GetLotByName&Exterior
+           // } else if (lotType != 0) {                          // GetLotByType                  
+          //  }else if (lotType != 0)
+          //  {                                             // GetAllLots
+                /** Dummy Return2 **/
+                Lot lot1 = new Lot() { LotId = 1, LotName = "Lot 1", LotImage = "image2.jpg", LotType = 1, LotDescript = "leather recliner", Exterior = false, Price = 1900.00m };
+                Lot lot2 = new Lot() { LotId = 2, LotName = "Lot 2", LotImage = "image1.jpg", LotType = 3, LotDescript = "picnic table", Exterior = true, Price = 190.00m };
+                
+                allLots.Add(lot1);
+                allLots.Add(lot2);
+            }
+            else
+            {
+                /** Dummy Return1 **/
+                Lot lot1 = new Lot() { LotId = 1, LotName = "Lot 1", LotImage = "image1.jpg", LotType = 1, LotDescript = "leather recliner", Exterior = false, Price = 1900.00m };
+                Lot lot2 = new Lot() { LotId = 2, LotName = "Lot 2", LotImage = "image2.jpg", LotType = 3, LotDescript = "picnic table", Exterior = true, Price = 190.00m };
+                
+                allLots.Add(lot1);
+                allLots.Add(lot2);
+
+            }
+            
+
+            return View(allLots);
+        }
+
+        public ActionResult CreateLot(int lotId, string lotName, string lotImage, string LotDescript, Boolean exterior, Decimal price)
+        {
+            return View();
         }
     }
 }
--- DFEItechFurniture/Models/Lot.cs
+++ DFEItechFurniture/Models/Lot.cs
@@ -10,7 +10,7 @@ namespace DFEItechFurniture.Models
         public int LotId { get; set; }
         public string LotName { get; set; }
         public string LotImage { get; set; }
-        //public Type LotType {get; set;}
+        public int LotType {get; set;} // make me Type LotType
         public string LotDescript { get; set; }
         public Boolean Exterior { get; set; }
         public Decimal Price { get; set; }
